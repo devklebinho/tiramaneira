afc734b774d810528589ee33ad512c863da2deee
devklebinho <contato@klebersilva.dev.br>
2024-05-05 15:40:09 -0300

    first commit

---
+++ Assets/Game/Scripts/UI/MainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+
+    }
+
+    public void OpenOptions()
+    {
+
+    }
+
+    public void CloseOptions()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Game/Scripts/Utilities/ConsoleWriter.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsoleWriter : MonoBehaviour
+{
+    public void WriteConsole(string message)
+    {
+        Debug.Log(message);
+    }
+}
