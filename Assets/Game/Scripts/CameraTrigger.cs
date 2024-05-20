5046ef7aac2d661640e252a831c3a7e97044b1e2
jvvitorcs <jvvitorcs97@gmail.com>
2024-05-19 21:07:41 -0300

    Ajuste Fase 1

--- Assets/Game/Scripts/CameraTrigger.cs
+++ Assets/Game/Scripts/CameraTrigger.cs
@@ -5,21 +5,15 @@ using UnityEngine;
 public class CameraTrigger : MonoBehaviour
 {
     public float newPlayerPositionX, newPlayerpositionY, newPlayerpositionZ;
-   // public Camera cam;
     private PlayerScript player;
-    // Start is called before the first frame update
     void Start()
     {
-        //cam = Camera.main;
         player = FindFirstObjectByType<PlayerScript>();
     }
-
-    // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            //cam.transform.position = newCameraPosition;
             player.transform.position = new Vector3(newPlayerPositionX, newPlayerpositionY, newPlayerpositionZ);
         }
     }
