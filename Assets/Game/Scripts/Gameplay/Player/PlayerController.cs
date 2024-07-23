488d59ec155cbe801318238d925fe8013db33798
SingleFarama <ricardoliradasilva16@gmail.com>
2024-07-23 18:47:53 -0300

    Teste do Novo Movimento
    Após algumas tentativas e acabei esquecendo de continuar com o projeto, mas este foi o que fiz até agora.
    
    Em não muito tempo eu estarei continuando com isso!
    
    Basicamente, eu criei dois novos scripts, uma grid nova e mais fácil de se utilizar e renomeei certas coisas para ser facilitada de ser encontrada e/ou utilizada.
    Também criei uma nova cena e novo player, chamado de Cena 1.1 e PlayerTest respectivamente.
    
    Após alguns testes eu não havia conseguido continuar muito, com eu preso em algumas coisas, que se possível, estarei falando com Kleber ou  alguém do resto do pessoal para ajudar no assunto.

---
+++ Assets/Game/Scripts/Gameplay/Player/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerControler : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap groundTilemap;
+    [SerializeField]
+    private Tilemap collisionTilemap;
+
+    private PlayerMovement controls;
+
+    private void Awake()
+    {
+        controls = new PlayerMovement();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    void Start()
+    {
+        controls.Player.Movement.performed += ctx => Move(ctx.ReadValue<Vector2>());
+    }
+
+    private void Move(Vector2 direction)
+    {
+        if (CanMove(direction))
+            transform.position += (Vector3)direction;
+    }
+
+    private bool CanMove(Vector2 direction)
+    {
+        Vector3Int gridPosition = groundTilemap.WorldToCell(transform.position + (Vector3)direction);
+        if (!groundTilemap.HasTile(gridPosition) || collisionTilemap.HasTile(gridPosition))
+            return false;
+        else return true;
+    }
+
+}
